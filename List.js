85eb9b355cd3019e3a9198231324b85e853f6bf8
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-03-05 14:37:50 +0200

    Create List.js

---
+++ List.js
@@ -0,0 +1,36 @@
+/*
+  create a new type called List with the following criteria:
+  - setting an index of that List to undefined deletes that element from the List.
+  - other APIs should be the same as the Array's APIs like map, reduce, and others
+*/
+
+class List extends Array {
+  constructor(...args){
+    super(...Array.from(args));
+    return new Proxy(this, {
+      set: (target, attr, val, context) => {
+        const idx = Number(attr);
+        if(!Number.isNaN(idx) && Number.isInteger(idx) && val === undefined){
+          target.splice(idx, 1);
+          return true;
+        }
+        return Reflect.set(target, attr, val, context);
+      },
+    })
+  }
+  
+  [Symbol.toPrimitive](){
+    return `[ ${this.join(', ')} ]`
+  }
+  
+  [Symbol.for('nodejs.util.inspect.custom')]() {
+    return [...this];
+  }
+}
+
+arr = List.from([1, 2, 3, 4]);
+console.log(arr);
+
+arr[1] = undefined;
+
+console.log(arr);
